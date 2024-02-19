2630f67bee8cf5047725ded6393b120f23f56b91
Thomas Piskorski <20681985+t8p@users.noreply.github.com>
2024-02-19 08:11:52 +0100

    Simple material 3 example with tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatAnchor, MatButton } from '@angular/material/button';
+import { MatChipListbox, MatChipOption } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButton,
+    MatAnchor,
+    MatChipListbox,
+    MatChipOption,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-material-3-experimental';
